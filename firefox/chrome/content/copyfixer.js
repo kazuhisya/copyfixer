f729f0fa09dfa238a2116f2ac9b9c6a8609c16d9
dingzhengkai <dingzhengkai@ifeidao.com>
2018-06-14 10:14:26 +0800

    firefox initial commit

--- firefox/chrome/content/copyfixer.js
+++ firefox/chrome/content/copyfixer.js
@@ -1,35 +1,59 @@
-var copyfixer = (function() {
+window.addEventListener('keydown', copyfixer, true);
 
-var self = {};
-
-self.copy = function() {
-    if (self.isSelected()) {
-        var controller = document.commandDispatcher.getControllerForCommand("cmd_copy");
-        if (controller.isCommandEnabled("cmd_copy")) controller.doCommand("cmd_copy");
-        return;
-    }
-
-    var crlf = (navigator.platform.indexOf("Win") != -1) ? "\r\n" : "\n";
-    var doc  = window._content.document;
-    var txt  = doc.title + crlf + doc.location.href + crlf + crlf;
-    var cpb  = Components.classes["@mozilla.org/widget/clipboardhelper;1"].getService(Components.interfaces.nsIClipboardHelper);
-    cpb.copyString(txt);
+function copyfixer(event) {
+    if (event.keyCode != 67) return;
+    var isWin = (navigator.platform.indexOf("Win") != -1);
+    var isMac = (navigator.platform.indexOf("Mac") != -1);
+    if ((!isMac && !event.ctrlKey) || (isMac && !event.metaKey)) return;
+    if (isSelected()) return;
+    var crlf = isWin ? "\r\n" : "\n";
+    var txt = document.title + crlf + document.location.href + crlf;
+    debugger
+    copyToClipboard(txt);
 }
 
-self.isSelected = function() {
-    var focusedWindow = document.commandDispatcher.focusedWindow;
-    var sel = focusedWindow.getSelection();
-    if (sel.rangeCount == 0) return false;
-    if (sel.rangeCount > 1)  return true;
+function isSelected() {
+    var sel = window.getSelection();
+    if (sel.rangeCount <= 0) return false;
+    if (sel.rangeCount > 1) return true;
 
     var range = sel.getRangeAt(0);
+    if (!range.collapsed) return true;
     if (range.startContainer != range.endContainer) return true;
-    if (range.startOffset    != range.endOffset)    return true;
+    if (range.startOffset != range.endOffset) return true;
+    if (document.activeElement.tagName.toLowerCase() != "body") return true;
+
     return false;
 }
 
-return self;
 
-})();
+function copyToClipboard(textToCopy, fallbackString) {
+    debugger
+    fallbackString = fallbackString || 'Copy URL and title'
 
+    return new Promise((resolve) => {
+        const textArea = document.createElement('textarea')
 
+        textArea.value = textToCopy
+        document.body.appendChild(textArea)
+        textArea.select()
+
+        const magicCopyFailed = () => {
+            document.body.removeChild(textArea)
+            window.prompt(fallbackString, textToCopy)
+            resolve()
+        }
+
+        try {
+            const successful = document.execCommand('copy')
+            if (!successful) {
+                return magicCopyFailed('copy command unavailable')
+            }
+            document.body.removeChild(textArea)
+            return resolve()
+        } catch (err) {
+            document.body.removeChild(textArea)
+            return magicCopyFailed(err)
+        }
+    })
+}
